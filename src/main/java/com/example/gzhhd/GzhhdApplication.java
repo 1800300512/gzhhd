21d3204fc1b32e04c327073a4c2999018a6cb0ba
180300512 <212697301@qq,com>
2021-08-05 10:17:44 +0800

    公众号后端

---
+++ src/main/java/com/example/gzhhd/GzhhdApplication.java
@@ -0,0 +1,13 @@
+package com.example.gzhhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GzhhdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GzhhdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/gzhhd/GzhhdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gzhhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GzhhdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
